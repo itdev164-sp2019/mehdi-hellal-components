dd7c38ac4bdaec38ca21dd1536fa0b1df391ba0a
[Mehdi Hellal] <[hellalm@gmatc.matc.edu]>
2019-04-24 16:05:31 -0500

    Completed activity 9

--- components/src/pages/index.js
+++ components/src/pages/index.js
@@ -6,6 +6,8 @@ import Image from "../gatsby-components/image"
 import SEO from "../gatsby-components/seo"
 import{Button, IconButton, Section} from "../components/Element"
 import {Search} from 'styled-icons/feather'
+import {ShareButtonList} from '../components/SocialShareButton'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
@@ -15,6 +17,13 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <ShareButtonList
+    addThisPubId="ra-5cc0b1d3cb9d02ab"
+    googleAnalyticsId="UA-763513-3"
+    contentTitle="Fake title"
+    contentImage="http://placekitten.com/g/300/700"
+    contentDescription=" Here is an article about science things, and maybe kittens."
+    />
     <Section flex justifyContent="space-evenly" m={2}>
       <Link to="/page-2/">Go to page 2</Link>
       <Button variant="primary">My Custom Button!</Button>
